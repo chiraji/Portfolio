7b824b5819c8f3ea7d8bbefa92ec4ab6c9997ab8
Chiraji Dilipun <hchiraji@gmail.com>
2025-08-08 16:33:00 +0530

    Updated

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
-// toggle icon navbar
-
+// Toggle icon navbar
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
+
 menuIcon.onclick = () => {
   menuIcon.classList.toggle("bx-x");
   navbar.classList.toggle("active");
 };
 
 // Scroll Sections Active Links
-
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
 
@@ -20,39 +19,35 @@ window.onscroll = () => {
     let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      navLinks.forEach((links) => {
-        links.classList.remove("active");
-        document
-          .querySelector("header nav a[href*=" + id + "]")
-          .classList.add("active");
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
       });
+      document
+        .querySelector("header nav a[href*=" + id + "]")
+        .classList.add("active");
     }
   });
 
   // Sticky navbar
-
-  let header = document.querySelector("header");
+  let header = document.querySelector(".header");
   header.classList.toggle("sticky", window.scrollY > 100);
 
-  //    remove toggle icon and navbar when click navbar link
-
+  // Remove toggle icon & navbar when link clicked (scroll)
   menuIcon.classList.remove("bx-x");
   navbar.classList.remove("active");
 };
 
-// scroll reveal
-
+// Scroll Reveal Animation
 ScrollReveal({
-//   reset: true,
   distance: "80px",
   duration: 2000,
-  delay: 200,
+  delay: 200
 });
 ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
 ScrollReveal().reveal(".home-img, .services-container, .portfolio-box, .contact form", { origin: "bottom" });
 ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
 ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
-ScrollReveal().reveal(".education-container, .education-box", { origin: "bottom" });
+
 
 
 
