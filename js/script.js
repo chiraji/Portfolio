4c4ca7eaebcb19a36868f217cc61b16e4bb96d4c
Chiraji Dilipun <hchiraji@gmail.com>
2025-08-10 14:10:00 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -43,7 +43,7 @@ ScrollReveal({
   delay: 200
 });
 ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
-ScrollReveal().reveal(".home-img, .services-container, .portfolio-box, .contact form", { origin: "bottom" });
+ScrollReveal().reveal(".home-img, .services-container, .portfolio-box, .certification-box, .education-box, .contact form", { origin: "bottom" });
 ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
 ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
 
